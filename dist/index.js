d82035d75ab1124f1f76a1a1c1cb68db6a58a33e
afeiship <1290657123@qq.com>
2020-07-27 21:57:24 +0800

    chore: do not compress

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,38 @@
  * name: @feizheng/glob-copy
  * description: Copy based on globby.
  * homepage: https://github.com/afeiship/glob-copy
- * version: 1.0.1
- * date: 2020-07-27T13:23:01.196Z
+ * version: 1.0.2
+ * date: 2020-07-27T13:57:17.399Z
  * license: MIT
  */
 
-"use strict";var fs=require("fs"),path=require("path"),glob=require("globby"),mkdirp=require("mkdirp"),copy=function(e,p){e.forEach(function(e){var r=path.join(p,path.dirname(e)),c=path.basename(e);mkdirp.sync(r),fs.createReadStream(e).pipe(fs.createWriteStream(path.join(r,c)))})},cp=function(e,c,p){glob(e,function(e,r){copy(r,c),p&&p(e,r)})},cpSync=function(e,r){copy(glob.sync(e),r)};module.exports=cp,module.exports.sync=cpSync;
+var fs = require('fs');
+var path = require('path');
+var glob = require('globby');
+var mkdirp = require('mkdirp');
+
+var copy = function (files, destFolder) {
+  files.forEach(function (filename) {
+    var folder = path.join(destFolder, path.dirname(filename));
+    var file = path.basename(filename);
+    mkdirp.sync(folder);
+
+    fs.createReadStream(filename).pipe(
+      fs.createWriteStream(path.join(folder, file))
+    );
+  });
+};
+
+var cp = function (pattern, destFolder, cb) {
+  glob(pattern, function (err, files) {
+    copy(files, destFolder);
+    if (cb) cb(err, files);
+  });
+};
+
+var cpSync = function (pattern, destFolder) {
+  copy(glob.sync(pattern), destFolder);
+};
+
+module.exports = cp;
+module.exports.sync = cpSync;
